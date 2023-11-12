7b8fa43356a39d1d5e009f402cf4a440e9a72a3a
Tam0721 <95025792+Tam0721@users.noreply.github.com>
2023-11-12 09:50:18 +0700

    Update 2023_09_29_172511_create_phong_table.php

--- database/migrations/2023_09_29_172511_create_phong_table.php
+++ database/migrations/2023_09_29_172511_create_phong_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->increments('id_phong');
             $table->string('ten_phong', 255);
             $table->boolean('phong_status')->default(0);
+            $table->unsignedTinyInteger('id_ca_hoc');
+            $table->foreign('id_ca_hoc')->references('id_ca_hoc')->on('ca_hoc')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedTinyInteger('id_loai_phong');
             $table->foreign('id_loai_phong')->references('id_loai_phong')->on('loai_phong')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedTinyInteger('id_co_so');
+            $table->foreign('id_co_so')->references('id_co_so')->on('co_so')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedTinyInteger('id_tang');
             $table->foreign('id_tang')->references('id_tang')->on('tang')->onDelete('cascade')->onUpdate('cascade');
             $table->unsignedTinyInteger('id_toa_nha');
