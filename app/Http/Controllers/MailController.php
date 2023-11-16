45d22b83073adab43b0728dafec8789c2d86b585
Đỗ Trọng Nhân <143280120+nhantrongd@users.noreply.github.com>
2023-11-16 14:14:25 +0700

    update form huy mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Mail\CancelRoom;
 use App\Models\User;
-use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
     //
-    function sendMail(){
-        $userMail = User::orderBy("id_user","desc")->limit(1)->get('email');
-        Mail::to($userMail)->send(new CancelRoom());
-        return redirect()->route('ls');
+    function sendMail(Request $request ,string $id){   
+        $liDo = $request->liDo;
+        $userMails = User::where('role', 1)->get('email');
+        foreach($userMails as $userMail){
+            Mail::to($userMail->email)->send(new CancelRoom($liDo));
+        }
+        return redirect()->route('ls')->with('success','Hủy thành công.');
     }
 
 }
